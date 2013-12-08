1d65667f0e57fa946b17ee94a89594578bc0fff0
Paweł Stiasny <pawelstiasny@gmail.com>
2013-12-08 16:34:58 +0100

    Basic interactive mode: open command.

--- src/testclient.c
+++ src/testclient.c
@@ -5,10 +5,25 @@
 
 int main(int argc, char **argv)
 {
-    int server_handle = fs_open_server("localhost");
+    int server_handle;
+    char input_buf[1024], filename[256];
+
+    server_handle = fs_open_server((argc < 2) ? "localhost" : argv[1]);
     printf("fs_open_server returned %d\n", server_handle);
-    printf("fs_open returned %d\n",
-        fs_open(server_handle, "Hello world from da client lib!\n", 16));
+
+    while (fgets(input_buf, sizeof(input_buf), stdin)) {
+        if (*input_buf == '\n')
+            continue;
+
+        if (1 == sscanf(input_buf, "open %255s", filename)) {
+            printf("fs_open returned %d\n",
+                   fs_open(server_handle, filename, 16));
+        } else {
+            printf("unknown command\n");
+        }
+    }
+
+
     printf("fs_close_server returned %d\n", fs_close_server(server_handle));
 
     return 0;
