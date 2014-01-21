0959e251ee125c3b6a9acc0012450ce1cad60b38
Colector1337 <colector1337@gmail.com>
2014-01-21 04:13:41 +0100

    Poprawiony skrypt testowy

--- tests/test.sh
+++ tests/test.sh
@@ -20,7 +20,7 @@ assert_file_eq() {
 
 
 ###############################################################################
-echo "TEST opening a file"
+echo "TEST opening a simple file"
 transaction <<TERM
 open tests/FILE1
 TERM
@@ -33,7 +33,7 @@ TERM
 
 
 ###############################################################################
-echo "TEST downloading a file"
+echo "TEST downloading a simple file"
 transaction <<TERM
 open tests/FILE1
 read /tmp/OUT 2048
@@ -52,7 +52,7 @@ assert_file_eq "downloaded file" /tmp/OUT < tests/FILE1
 
 
 ###############################################################################
-echo "TEST downloading a part of file"
+echo "TEST downloading a part of simple file"
 transaction <<TERM
 open tests/FILE1
 read /tmp/OUT 10
@@ -73,7 +73,7 @@ TERM
 
 
 ###############################################################################
-echo "TEST downloading inner part of file"
+echo "TEST downloading inner part of simple file"
 transaction <<TERM
 open tests/FILE1
 lseek 11 0
@@ -98,7 +98,7 @@ TERM
 
 
 ###############################################################################
-echo "TEST uploading a file"
+echo "TEST uploading a simple file"
 transaction <<TERM
 open /tmp/OUT
 write tests/FILE1 1024
@@ -115,6 +115,197 @@ assert_file_eq "uploaded file" /tmp/OUT < tests/FILE1
 
 
 ###############################################################################
+echo "TEST opening a file with plain text"
+transaction <<TERM
+open tests/FILE2
+TERM
+
+assert_file_eq "client messages" /tmp/cout <<TERM
+fs_open_server returned 0
+fs_open returned 3
+fs_close_server returned 0
+TERM
+
+
+###############################################################################
+echo "TEST downloading a file with plain text"
+transaction <<TERM
+open tests/FILE2
+read /tmp/OUT 4096
+close
+TERM
+
+assert_file_eq "client messages" /tmp/cout <<TERM
+fs_open_server returned 0
+fs_open returned 3
+fs_read returned 32
+fs_close returned 0
+fs_close_server returned 0
+TERM
+
+assert_file_eq "downloaded file" /tmp/OUT < tests/FILE1
+
+
+###############################################################################
+echo "TEST downloading a part of file with plain text"
+transaction <<TERM
+open tests/FILE2
+read /tmp/OUT 100
+close
+TERM
+
+assert_file_eq "client messages" /tmp/cout <<TERM
+fs_open_server returned 0
+fs_open returned 3
+fs_read returned 10
+fs_close returned 0
+fs_close_server returned 0
+TERM
+
+assert_file_eq "downloaded file" /tmp/OUT <<TERM
+to dane te
+TERM
+
+
+###############################################################################
+echo "TEST downloading inner part of file with plain text"
+transaction <<TERM
+open tests/FILE2
+lseek 50 0
+lseek -1 10
+read /tmp/OUT 5
+close
+TERM
+
+assert_file_eq "client messages" /tmp/cout <<TERM
+fs_open_server returned 0
+fs_open returned 3
+fs_lseek returned 11
+fs_lseek returned 10
+fs_read returned 5
+fs_close returned 0
+fs_close_server returned 0
+TERM
+
+assert_file_eq "downloaded file" /tmp/OUT <<TERM
+stowe
+TERM
+
+
+###############################################################################
+echo "TEST uploading a file with plain text"
+transaction <<TERM
+open /tmp/OUT
+write tests/FILE2 1024
+TERM
+
+assert_file_eq "client messages" /tmp/cout <<TERM
+fs_open_server returned 0
+fs_open returned 3
+fs_write returned 32
+fs_close_server returned 0
+TERM
+
+assert_file_eq "uploaded file" /tmp/OUT < tests/FILE1
+
+
+###############################################################################
+echo "TEST opening a file with bad test case"
+transaction <<TERM
+open tests/FILE3
+TERM
+
+assert_file_eq "client messages" /tmp/cout <<TERM
+fs_open_server returned 0
+fs_open returned 3
+fs_close_server returned 0
+TERM
+
+
+###############################################################################
+echo "TEST downloading a file with bad test case"
+transaction <<TERM
+open tests/FILE3
+read /tmp/OUT 4096
+close
+TERM
+
+assert_file_eq "client messages" /tmp/cout <<TERM
+fs_open_server returned 0
+fs_open returned 3
+fs_read returned 32
+fs_close returned 0
+fs_close_server returned 0
+TERM
+
+assert_file_eq "downloaded file" /tmp/OUT < tests/FILE1
+
+
+###############################################################################
+echo "TEST downloading a part of file with bad test case"
+transaction <<TERM
+open tests/FILE3
+read /tmp/OUT 100
+close
+TERM
+
+assert_file_eq "client messages" /tmp/cout <<TERM
+fs_open_server returned 0
+fs_open returned 3
+fs_read returned 10
+fs_close returned 0
+fs_close_server returned 0
+TERM
+
+assert_file_eq "downloaded file" /tmp/OUT <<TERM
+to dane te
+TERM
+
+
+###############################################################################
+echo "TEST downloading inner part of file with bad test case"
+transaction <<TERM
+open tests/FILE3
+lseek 50 0
+lseek -1 10
+read /tmp/OUT 5
+close
+TERM
+
+assert_file_eq "client messages" /tmp/cout <<TERM
+fs_open_server returned 0
+fs_open returned 3
+fs_lseek returned 11
+fs_lseek returned 10
+fs_read returned 5
+fs_close returned 0
+fs_close_server returned 0
+TERM
+
+assert_file_eq "downloaded file" /tmp/OUT <<TERM
+stowe
+TERM
+
+
+###############################################################################
+echo "TEST uploading a file with bad test case"
+transaction <<TERM
+open /tmp/OUT
+write tests/FILE3 1024
+TERM
+
+assert_file_eq "client messages" /tmp/cout <<TERM
+fs_open_server returned 0
+fs_open returned 3
+fs_write returned 32
+fs_close_server returned 0
+TERM
+
+assert_file_eq "uploaded file" /tmp/OUT < tests/FILE1
+
+
+###############################################################################
+
 if [ $FAILED_ASSERTIONS -gt 0 ]; then
     echo "$FAILED_ASSERTIONS assertions failed" >&2
     exit 1
